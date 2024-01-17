88d1de1c26907d2c178f70115ef81eeeb062fc5d
FIRST_NAME LAST_NAME <henry49@nycstudents.net>
2024-01-17 10:51:35 -0500

    commit 3

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-let timesRolled;
+let timesRoll;
+let numOfDice;
 function handleSubmit(){
     let input = document.getElementById("userinput").value;
     console.log(input);
 }
+function updateRadio(num){
+    numOfDice = num;
+    document.getElementById('dice2').style.opacity = 1;
+    document.getElementById('dice1').style.opacity = 1;
+    document.getElementById('dice3').style.opacity = 1;
+
+    if(numOfDice == 1){
+        document.getElementById('dice3').style.opacity = 0;
+        document.getElementById('dice2').style.opacity = 0;
+    } else if(numOfDice == 2){
+        document.getElementById('dice3').style.opacity = 0;
+    }
+}
+
+function rollDice(){
+    
+}
+
 
