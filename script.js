f6e9b2df7bdd815a432737fa82b9bf01db6f5751
jingz <Henryz49@nycstudents.net>
2024-01-16 21:58:07 -0500

    commit 2

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-let rollhandler = document.getElementById("rollHandler");
 let timesRolled;
-
-rollhandler.addEventListener("submit", (e) => {
-    e.preventDefault();
-    
-    let rollAmount = document.getElementById("rollAmount")
-
-    if (typeof rollAmount.value == "number" ) {
-        console.log(`This form has a roll amount of ${rollAmount.value}`);
-        timesRolled = rollAmount.value;
-        console.log(timesRolled);
-
-    } else {
-        alert("Ensure you input a number!");
-    }
-});
+function handleSubmit(){
+    let input = document.getElementById("userinput").value;
+    console.log(input);
+}
 
